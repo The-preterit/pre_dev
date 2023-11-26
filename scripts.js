3e7fcd02f88131544577f55e9738e99e31dfa2ca
pre_dev <sylvaingossou@gmail.com>
2023-11-26 14:26:42 +0000

    Ajout d;une maquette et cretion de la fonction qui defile les skills

--- scripts.js
+++ scripts.js
@@ -1,3 +1,45 @@
 
 let zoneText = document.getElementById("textVariable");
+console.log(zoneText)
+
+let button = document.querySelector("button")
+
+let cg = 0
+
+function iteration(itr){
+    switch(itr){
+        case 0:
+            zoneText.innerText = "Algorithmic"
+            break
+
+        case 1:
+            zoneText.innerText = "C language"
+            break
+            
+        case 2:
+            zoneText.innerText = "Web Site developper(Backend junior)"
+            break
+            
+        case 3:
+            zoneText.innerText = "Mobile application developper"
+            break
+            
+        case 4:
+            zoneText.innerText = "Git nd Github"
+            break
+            
+        case 5:
+            zoneText.innerText = "Dreaming to be Data scientist"
+            break
+        default:
+            cg = 0
+            break
+    }
+    cg++
+}
+
+button.addEventListener("click", () => {
+    console.log("click")
+    iteration(cg)
+})
 
